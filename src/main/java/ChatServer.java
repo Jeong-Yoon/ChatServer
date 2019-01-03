fb0740eb31c5801cbf29aabbbfe55c96c2e2bbfa
jeong <jjung4025@naver.com>
2019-01-03 16:20:22 +0900

    Revert "close #1 ChatServer 클래스 구현"
    This reverts commit d512818e9433c1d9f6e198ecdb2dc68e2363181f.

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,31 +1,3 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class ChatServer {
-    private int port;
-    private ChatHouse chatHouse;
 
-    public ChatServer(int port) {
-        this.port = port;
-        chatHouse = new ChatHouse();
-    }
-
-    public void run(){
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(port);
-            while (true){
-                Socket socket = serverSocket.accept();
-                ChatServerHandler chatServerHandler = new ChatServerHandler(socket,chatHouse);
-                chatServerHandler.start();
-            }
-        } catch (IOException e) {
-            System.out.println("오류가 발생하였습니다.");
-        }finally {
-            try {
-                serverSocket.close();
-            }catch (Exception ignore){}
-        }
-    }
 }
